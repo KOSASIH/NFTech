ee9fc3d0f0b11cae184fb98107d5168bb1c4aa17
KOSASIH <kosasihg88@gmail.com>
2024-10-30 08:25:21 +0700

    Create Notification.js

---
+++ src/components/Notification.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Notification = () => {
+    return (
+        <div className="notification">
+            <p>Get notified about new updates and features!</p>
+            <button>Subscribe</button>
+        </div>
+    );
+};
+
+export default Notification;
