7b43ea16d6ca82eada9123e2a29a26bad867ef4c
KOSASIH <kosasihg88@gmail.com>
2024-10-30 08:22:40 +0700

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <h1>NFTech</h1>
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/about">About</Link>
+                <Link to="/contact">Contact</Link>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
